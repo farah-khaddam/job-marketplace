a152797c26faea3727cf20db1f0d8f4bf482bd26
alaaayrout <aalaaayrout02@gmail.com>
2026-07-14 23:01:10 +0300

    test emails

--- backend/jobportal/email_backend.py
+++ backend/jobportal/email_backend.py
@@ -1,34 +1,28 @@
-import smtplib
-import socket
-from django.core.mail.backends.smtp import EmailBackend as DjangoSMTPBackend
+import resend
+from django.conf import settings
+from django.core.mail.backends.base import BaseEmailBackend
+
+resend.api_key = settings.RESEND_API_KEY
 
 
-class EmailBackend(DjangoSMTPBackend):
-    def open(self):
-        if self.connection:
-            return False
-        try:
-            self.connection = smtplib.SMTP(timeout=self.timeout)
-            # نجبر الاتصال يستخدم IPv4 فقط
-            addr_info = socket.getaddrinfo(
-                self.host, self.port, socket.AF_INET, socket.SOCK_STREAM
-            )
-            self.connection._host = self.host
-            sock = socket.create_connection(addr_info[0][4], timeout=self.timeout)
-            self.connection.sock = sock
-            self.connection.file = sock.makefile("rb")
-            (code, msg) = self.connection.getreply()
-            if code != 220:
-                self.connection.close()
-                raise smtplib.SMTPConnectError(code, msg)
-            self.connection.ehlo()
-            if self.use_tls:
-                self.connection.starttls()
-                self.connection.ehlo()
-            if self.username and self.password:
-                self.connection.login(self.username, self.password)
-            return True
-        except OSError:
-            if not self.fail_silently:
-                raise
-            return False
+class EmailBackend(BaseEmailBackend):
+    def send_messages(self, email_messages):
+        count = 0
+        for message in email_messages:
+            html = None
+            for content, mimetype in getattr(message, "alternatives", []):
+                if mimetype == "text/html":
+                    html = content
+            try:
+                resend.Emails.send({
+                    "from": message.from_email,
+                    "to": message.to,
+                    "subject": message.subject,
+                    "text": message.body,
+                    "html": html or message.body,
+                })
+                count += 1
+            except Exception as e:
+                if not self.fail_silently:
+                    raise
+        return count
